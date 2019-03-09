313e2c639be3b2445b3e7d50a630f61c01dcb0bd
Yuan <1398095376@qq.com>
2019-03-09 21:37:07 +0800

    添加junit单元测试

---
+++ src/com/yiban/test/JunitTest.java
@@ -0,0 +1,12 @@
+package com.yiban.test;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class JunitTest extends TestCase {
+
+	@Test
+	public void testPrintMessage() {
+		System.out.println("test");
+	}
+}
